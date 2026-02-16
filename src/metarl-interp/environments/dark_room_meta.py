5d9c22a6166c41ec60534f6786415923141abef3
Arthur <relentlessviper10@gmail.com>
2026-02-16 16:51:39 +0300

    Update RL2DarkRoom. Added additional information about cumulative returns for each trial as well as separate  flag

--- src/metarl-interp/environments/dark_room_meta.py
+++ src/metarl-interp/environments/dark_room_meta.py
@@ -1,4 +1,6 @@
 import gymnasium as gym
+import numpy as np
+from copy import deepcopy
 
 class RL2DarkRoom(gym.Wrapper):
     """
@@ -8,22 +10,24 @@ class RL2DarkRoom(gym.Wrapper):
     def __init__(
         self,
         env,
-        episodes_per_trial=3
+        trials_per_episode=3,
     ):
         super().__init__(env)
-        self.base_env = env.unwrapped
-        self.episodes_per_trial = episodes_per_trial
-        self.episode_counter = 0
+        self.main_env = env # Do not remove TimeLimit, etc.
+        self.trials_per_episode = trials_per_episode
+        self.trial_counter = 0
+        self.cumulative_reward_per_trial = np.zeros((trials_per_episode,), dtype=np.float32)
     
     def reset(self, **kwargs):
         """
-        Reset the environment (the agent position). If it was the last episode of a trial, then the new goal position will be sampled. Otherwise, the goal position remains the same.
+        Reset the environment (the agent position). If it was the last trila of an episode, then the new goal position will be sampled. Otherwise, the goal position remains the same.
         """
-        if self.episode_counter == 0:
-            obs, info = self.base_env.reset(**kwargs)
-            self.base_env.goal_pos = self.base_env.generate_goal()
-        else:
-            obs, info = self.base_env.reset(**kwargs)
+        if self.trial_counter == 0:
+            self.main_env.unwrapped.goal_pos = self.main_env.unwrapped.generate_goal()
+            self.cumulative_reward_per_trial = np.zeros((self.trials_per_episode,), dtype=np.float32)
+
+        obs, info = self.main_env.reset(**kwargs)
+        info["trial_done"] = False
             
         return obs, info
     
@@ -31,17 +35,35 @@ class RL2DarkRoom(gym.Wrapper):
         """
         Modified RL^2 step method. If an episode within the trial ends, it will return a new episode state with `False` done signal. Otherwise, the usual output is sent.
         """
-        next_obs, reward, terminated, truncated, info = self.base_env.step(action)
+        next_obs, reward, terminated, truncated, info = self.main_env.step(action)
+        self.cumulative_reward_per_trial[self.trial_counter] += reward
         done = terminated or truncated
 
-        if done:
-            self.episode_counter += 1
 
-            if self.episode_counter >= self.episodes_per_trial:
-                self.episode_counter = 0
+        if done:
+            info["cumulative_reward_per_trial"] = self.cumulative_reward_per_trial
+            info["current_trial"] = deepcopy(self.trial_counter)
+            self.trial_counter += 1
+
+            if self.trial_counter >= self.trials_per_episode:
+                self.trial_counter = 0
+                info["trial_done"] = True
                 return next_obs, reward, terminated, truncated, info
             else:
-                next_obs, info = self.reset()
+                next_obs, _ = self.reset()
+                info["trial_done"] = True
                 return next_obs, reward, False, False, info
         
+        info["trial_done"] = False
         return next_obs, reward, terminated, truncated, info
+    
+# import toymeta
+# env = RL2DarkRoom(
+#     gym.make("Dark-Room-3x3-v0"),
+#     trials_per_episode=4,
+# )
+# obs, info = env.reset()
+
+# for step in range(45):
+#     obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
+#     print(step + 1, obs, reward, terminated, truncated, info)
