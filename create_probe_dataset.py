026b9e60c390032e7f41d02b66485b2490941edc
Arthur <relentlessviper10@gmail.com>
2026-02-27 20:59:46 +0300

    Update  - Now able to create dataset for 2 modes: 'one_for_all_trials' or 'one_for_each_trial'

--- create_probe_dataset.py
+++ create_probe_dataset.py
@@ -1,3 +1,4 @@
+# %%
 import os
 import random
 from collections import deque
@@ -32,6 +33,8 @@ class DatasetCollectionConfig:
     seed: int = 1
     num_episodes: int = 5_000
     num_trials: int = 3
+    probe_type: str = "one_for_all_trials"
+    """The type of probe we want to train. Can be either 'one_for_all_trials' or 'one_for_each_trial'"""
     env_id: str = "Dark-Room-5x5-v0"
     save_path: str = None
     model_checkpoint_path: str = None
@@ -41,16 +44,15 @@ class DatasetCollectionConfig:
     
     def __post_init__(self):
         self.dataset_len = gym.make(self.env_id).spec.max_episode_steps * self.num_trials * self.num_episodes # 5x5 setting: 3*15*5000 = 225000
+        if self.model_checkpoint_path is None:
+            raise ValueError("`model_checkpoint_path` must be filled")
         if not self.save_path:
             self.save_path = f"datasets/{self.dataset_name}"
 
 def make_env(env_id, num_trials):
-    def thunk():
-        env = gym.make(env_id)
-        env = RL2DarkRoom(env, trials_per_episode=num_trials)
-        #env = gym.wrappers.RecordEpisodeStatistics(env)
-        return env
-    return thunk
+    env = gym.make(env_id)
+    env = RL2DarkRoom(env, trials_per_episode=num_trials)
+    return env
 
 def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
     torch.nn.init.orthogonal_(layer.weight, std)
@@ -61,7 +63,7 @@ class Agent(nn.Module):
     def __init__(self, envs, hidden_size, num_layers):
         super().__init__()
         self.in_proj = nn.Sequential(
-            layer_init(nn.Linear(np.prod(envs.single_observation_space.shape), hidden_size)),
+            layer_init(nn.Linear(np.prod(envs.observation_space.shape), hidden_size)),
             nn.ReLU(),
         )
         self.lstm = nn.GRU(hidden_size, hidden_size, num_layers=num_layers)
@@ -70,7 +72,7 @@ class Agent(nn.Module):
                 nn.init.constant_(param, 0)
             elif "weight" in name:
                 nn.init.orthogonal_(param, 1.0)
-        self.actor = layer_init(nn.Linear(hidden_size, envs.single_action_space.n), std=0.01)
+        self.actor = layer_init(nn.Linear(hidden_size, envs.action_space.n), std=0.01)
         self.critic = layer_init(nn.Linear(hidden_size, 1), std=1)
 
     def get_state(self, x, lstm_state, done):
@@ -128,66 +130,74 @@ def onehot_to_xy(onehot_tensor, grid_size=5):
 
 @draccus.wrap()
 def collect_trajectories(args: DatasetCollectionConfig):
-    args = DatasetCollectionConfig()
-    args.model_checkpoint_path = "checkpoints/Dark-Room-5x5-v0__hidden-512_envs-128_3-trials__1__1771760503_best.pt"
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
 
     device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
 
-    env = gym.vector.SyncVectorEnv([make_env(args.env_id, args.num_trials)])
-
+    env = make_env(args.env_id, args.num_trials)
     agent = Agent(env, args.hidden_size, args.num_layers)
     agent.load_state_dict(torch.load(args.model_checkpoint_path, weights_only=True)["model_state_dict"])
     agent.eval()
-
-    next_obs, info = env.reset(seed=args.seed)
-    next_obs = torch.Tensor(next_obs).to(device)
-    next_done = torch.zeros(env.num_envs).to(device)
-    next_lstm_state = torch.zeros(agent.lstm.num_layers, env.num_envs, agent.lstm.hidden_size).to(device)
-
     actions = []
     hidden_states = []
     observations = []
-    trial_data = [[] for _ in range(args.num_trials)]
+
+    if args.probe_type == "one_for_all_trials":
+        trial_data = [[]]
+    elif args.probe_type == "one_for_each_trial":
+        trial_data = [[] for _ in range(args.num_trials)]
+    else:
+        raise ValueError("`probe_type` can either be 'one_for_all_trials' or `one_for_each_trial`")
 
     with torch.no_grad():
-        for step in trange(args.dataset_len):
-            action, _, _, _, next_lstm_state = agent.get_action_and_value(
-                next_obs,
-                next_lstm_state,
-                next_done,
-            )
-            hidden_states += [next_lstm_state.reshape((-1,))]
-            actions += [action.reshape(())]
-            observations += [next_obs.reshape((-1,))]
+        for episode in trange(args.num_episodes):
+            next_obs, info = env.reset()
+            #print(env.unwrapped.goal_pos)
+            next_obs = torch.Tensor(next_obs).to(device)
+            next_done = torch.zeros(1).to(device)
+            next_lstm_state = torch.zeros(agent.lstm.num_layers, 1, agent.lstm.hidden_size).to(device)
 
-            trial_counter = env.envs[0].trial_counter
-            next_obs, reward, terminated, truncated, info = env.step(action.cpu().numpy())
-            next_done = np.logical_or(terminated, truncated)
-            next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(next_done).to(device)
+            for step in range(env.spec.max_episode_steps * env.trials_per_episode):
+                action, _, _, _, next_lstm_state = agent.get_action_and_value(
+                    next_obs,
+                    next_lstm_state,
+                    next_done,
+                )
+                hidden_states += [next_lstm_state.reshape((-1,))]
+                actions += [action.reshape(())]
+                observations += [next_obs.reshape((-1,))]
 
-            if info["trial_done"][0]:
-                observations[0][:25] = torch.Tensor([0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
-                trial_data[trial_counter] += [
-                    {
-                        "hidden_state": hidden_states,
-                        "action": actions,
-                        "observation": observations,
-                    }
-                ]
-                hidden_states, actions, observations = [], [], []
+                trial_counter = env.trial_counter if args.probe_type == "one_for_each_trial" else 0
+                next_obs, reward, terminated, truncated, info = env.step(action.item())
+                next_done = int(terminated or truncated)
+                next_obs, next_done = torch.Tensor(next_obs.reshape((1, -1))).to(device), torch.Tensor([next_done]).to(device)
+
+                if info["trial_done"]:
+                    trial_data[trial_counter] += [
+                        {
+                            "hidden_state": hidden_states,
+                            "action": actions,
+                            "observation": observations,
+                            "goal_pos": env.unwrapped.goal_pos,
+                        }
+                    ]
+                    hidden_states, actions, observations = [], [], []
+                
+                if terminated or truncated:
+                    break
 
         print("Data collection is finished.")
+
         # Reverse: define a grid with all first future actions for each hidden state
-        grid_size = env.envs[0].unwrapped.size ** 2
+        grid_size = env.unwrapped.size ** 2
         for idx, trial in enumerate(trial_data):
             for cur_trial in trial:
                 grid_state = torch.ones(
                     (
-                        env.envs[0].unwrapped.size,
-                        env.envs[0].unwrapped.size,
+                        env.unwrapped.size,
+                        env.unwrapped.size,
                     )
                 ).to(device) * -1 # -1 denotes a cell that was never visited
                 hidden_states = cur_trial["hidden_state"]
@@ -202,8 +212,7 @@ def collect_trajectories(args: DatasetCollectionConfig):
                         reversed(observations),
                     )
                 ):
-                    
-                    current_pos = onehot_to_xy(observation[:grid_size], env.envs[0].unwrapped.size)
+                    current_pos = onehot_to_xy(observation[:grid_size], env.unwrapped.size)
                     grid_state[current_pos[0]][current_pos[1]] = action
                     grid_states.appendleft(grid_state.clone())
                 
@@ -225,18 +234,22 @@ def collect_trajectories(args: DatasetCollectionConfig):
     os.makedirs(args.save_path, exist_ok=True)
     for i, dataset in enumerate(trial_datasets):
         all_tensors = dataset.tensors
-
-        torch.save(all_tensors, f'{args.save_path}/trial_{i}_dataset.pt')
-
         metadata = {
             'trial_idx': i,
             'num_samples': len(dataset),
             'tensor_shapes': [t.shape for t in all_tensors],
             'tensor_dtypes': [t.dtype for t in all_tensors]
         }
-        torch.save(metadata, f'{args.save_path}/trial_{i}_metadata.pt')
 
-        print(f"Saved trial {i} dataset:")
+        if args.probe_type == "one_for_all_trials":
+            torch.save(all_tensors, f'{args.save_path}/all_trials_dataset.pt')
+            torch.save(metadata, f'{args.save_path}/all_trials_metadata.pt')
+            print(f"Saved dataset for all trials:")
+        else:
+            torch.save(all_tensors, f'{args.save_path}/trial_{i}_dataset.pt')
+            torch.save(metadata, f'{args.save_path}/trial_{i}_metadata.pt')
+            print(f"Saved trial {i} dataset:")
+
         print(f"  - Location: {args.save_path}/trial_{i}_dataset.pt")
         print(f"  - Samples: {len(dataset)}")
         print(f"  - Shapes: {[t.shape for t in all_tensors]}")
